b0197bb7e7546f5f9883a00ad636532e149ae3e9
pyavchik <pyavchik@gmail.com>
2018-11-11 20:24:15 +0200

    added for i loop example

--- Section11-Javascript-Loops/ex_2_loops__Annoy-omatic_app/app.js
+++ Section11-Javascript-Loops/ex_2_loops__Annoy-omatic_app/app.js
@@ -7,7 +7,7 @@
 
 // Version 2
 var answer = prompt('are we there yet?');
-while (answer.indexOf('yes') === -1){
+while (answer.toLowerCase().indexOf('yes') === -1){
     var answer = prompt("are we there yet?");
 }
 alert('YAY, We made it!');
