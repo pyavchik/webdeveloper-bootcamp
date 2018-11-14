0d8d28d6e4584fc55480a37d978a4d2d557e8ac9
pyavchik <pyavchik@gmail.com>
2018-11-14 17:38:30 +0200

    added forEach array iteration example

---
+++ Section11-Javascript-Loops/ex_7_array_iteration_foreach/app.js
@@ -0,0 +1,20 @@
+var colors = ["red", "orange", "yellow", "green"];
+
+colors.forEach(function (color) {
+    console.log(color);
+});
+
+var students = ["sdnt1", "sdnt2", "sdnt3","sdnt4", "sdnt5"];
+students.forEach(function (student) {
+    console.log(student);
+});
+
+const cars = ["Mercedes", "Bently", "Mazda", "Moskwitch", "Matheratty", "Mibah", ];
+cars.forEach(function (car) {
+    console.log(car);
+});
+
+const fruits = ["apple", "banane", "grape", "strabery", "lemon", "waterlemon"];
+fruits.forEach(function (fruit) {
+    console.log(fruit);
+});
