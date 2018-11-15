2a8e11accc6748612b8cb182f4a5027bcbbd3624
pyavchik <pyavchik@gmail.com>
2018-11-15 14:24:48 +0200

    added creating objects examples

--- Section14-Javascript-Objects/ex_1_object/app.js
+++ Section14-Javascript-Objects/ex_1_object/app.js
@@ -1,5 +1,17 @@
+// Creating objects
+
+const dog = {};
+dog.name = "Rusty";
+dog.bread = "Mut";
+dog.age = 2;
+
 const person = {
     name: "Travis",
     age: 21,
     city: "LA"
 };
+
+const cat = new Object();
+cat.name = "Murzik";
+cat.bread = "Mut";
+cat.age = 4;
