995eca207fb22162a1199e97d52fbfe25ea6b40f
pyavchik <pyavchik@gmail.com>
2018-11-17 10:31:12 +0200

    added object exercise 1

---
+++ Section14-Javascript-Objects/ex_4_object_exercise_1/app.js
@@ -0,0 +1,15 @@
+var someObject = {};
+console.log(someObject);
+
+someObject._name = "Hedwig";
+console.log(someObject);
+
+someObject.age = 6;
+console.log(someObject);
+
+var prop = "color";
+someObject[prop] = "red";
+console.log(someObject);
+
+// someObject.123 = true; we cant do like this
+
