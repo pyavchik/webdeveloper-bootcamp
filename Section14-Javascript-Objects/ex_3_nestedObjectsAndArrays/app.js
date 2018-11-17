191c620302707ad1ee989f0d67748c66cec2be11
pyavchik <pyavchik@gmail.com>
2018-11-17 10:10:28 +0200

    nested objects and arrays example added

---
+++ Section14-Javascript-Objects/ex_3_nestedObjectsAndArrays/app.js
@@ -0,0 +1,15 @@
+var posts = [
+    {
+        title: "Cats are mediocre",
+        author: "Colt",
+        comments: ["Awesome post", "Terrible post"]
+    },
+    {
+        title: "Cats are actually awesome",
+        author: "Colt",
+        comments: ["<3", "I hate you"]
+    },
+];
+
+console.log(posts.length);
+console.log(posts[1].comments[1]);
