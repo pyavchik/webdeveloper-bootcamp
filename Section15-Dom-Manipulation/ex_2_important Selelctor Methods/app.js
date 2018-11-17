629068bcc900aff8efa01510b2d09ee05a015117
pyavchik <pyavchik@gmail.com>
2018-11-18 01:52:33 +0200

    added important selector methods

---
+++ Section15-Dom-Manipulation/ex_2_important Selelctor Methods/app.js
@@ -0,0 +1,18 @@
+// By ID
+console.log(document.getElementById("highlight"));
+
+// By className
+console.log(document.getElementsByClassName("bolded")[0]);
+console.log(document.getElementsByClassName("bolded")[0]);
+
+// By tagname
+console.log(document.getElementsByTagName("li"));
+console.log(document.getElementsByTagName("body")[0]);
+
+// querySelector
+console.log(document.querySelector("#highlight"));
+
+console.log(document.querySelector("li a.special"));
+
+// querySelectorAll
+console.log(document.querySelectorAll(".bolded")[0]);
