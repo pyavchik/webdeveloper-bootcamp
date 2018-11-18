6b5953669075c4d6d11ff5d5ca48e02a51ecde9e
pyavchik <pyavchik@gmail.com>
2018-11-18 23:01:46 +0200

    added one more exemple to exercise

--- Section15-Dom-Manipulation/ex_3_exercise_with_selectors/app.js
+++ Section15-Dom-Manipulation/ex_3_exercise_with_selectors/app.js
@@ -1,5 +1,7 @@
 // come up with 4 different ways to select the first <p> tag
 console.log(document.getElementById("first"));
-console.log(document.getElementsByClassName("special")[0]);
-console.log(document.getElementsByTagName("p")[0]);
 console.log(document.querySelector("#first"));
+console.log(document.getElementsByClassName("special")[0]);
+console.log(document.querySelector(".special"));
+console.log(document.getElementsByTagName("p")[0]);
+console.log(document.querySelector("h1 + p"));
