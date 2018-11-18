95cef8aa6701d4cf7f6a2d9db060782dc331c457
pyavchik <pyavchik@gmail.com>
2018-11-19 00:01:09 +0200

    added javascript manipulating syle example

---
+++ Section15-Dom-Manipulation/ex_4_manipulating_stile_example/app.js
@@ -0,0 +1,9 @@
+const p = document.querySelector("p");
+console.log(p.classList);
+p.classList.add("big");
+console.log(p.classList);
+p.classList.remove("big");
+console.log(p.classList);
+p.classList.toggle("big");
+console.log(p.classList);
+
