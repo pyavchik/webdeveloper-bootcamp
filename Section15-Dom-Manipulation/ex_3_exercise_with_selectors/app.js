875b0f0b077334830a2a5e9dcc69bbbf413c94a9
pyavchik <pyavchik@gmail.com>
2018-11-18 22:54:02 +0200

    exercise with selectors added

---
+++ Section15-Dom-Manipulation/ex_3_exercise_with_selectors/app.js
@@ -0,0 +1,5 @@
+// come up with 4 different ways to select the first <p> tag
+console.log(document.getElementById("first"));
+console.log(document.getElementsByClassName("special")[0]);
+console.log(document.getElementsByTagName("p")[0]);
+console.log(document.querySelector("#first"));
