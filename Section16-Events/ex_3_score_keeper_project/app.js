b7740e8a0d8d569907663657478ab8e40735ecdf
pyavchik <pyavchik@gmail.com>
2018-11-20 23:46:32 +0200

    added second part of score project

--- Section16-Events/ex_3_score_keeper_project/app.js
+++ Section16-Events/ex_3_score_keeper_project/app.js
@@ -1,10 +1,13 @@
 const p1Button = document.querySelector("#player1");
 let p1Score = 0;
 let p2Score = 0;
-let p1Display = document.querySelector("#p1Display")
-let p2Display = document.querySelector("#p2Display")
+let p1Display = document.querySelector("#p1Display");
+let p2Display = document.querySelector("#p2Display");
 const p2Button = document.getElementById("player2");
 const resetButton = document.getElementById("reset");
+var numInput = document.querySelector("input[type='number']");
+//var p = document.querySelector("p")
+var winningScoreDisplay = document.querySelector("p span");
 
 let gameOver = false;
 let winningScore = 5;
@@ -15,7 +18,8 @@ p1Button.addEventListener("click", function () {
     if(!gameOver){
         p1Score++;
         if(p1Score===winningScore){
-            console.log("Game over!!!")
+            p1Display.classList.add("winner");
+            console.log("Game over!!!");
             gameOver=true;
         }
         p1Display.textContent = p1Score;
@@ -26,9 +30,30 @@ p2Button.addEventListener("click", function () {
     if(!gameOver){
         p2Score++;
         if(p2Score===winningScore){
-            console.log("Game over!!!")
+            p2Display.classList.add("winner");
+            console.log("Game over!!!");
             gameOver=true;
         }
         p2Display.textContent = p2Score;
     }
+});
+
+resetButton.addEventListener("click", function () {
+   reset();
+});
+
+function reset() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
+
+numInput.addEventListener("change", function () {
+    winningScoreDisplay.textContent = numInput.value;
+    winningScore = Number(numInput.value);
+    reset();
 });
