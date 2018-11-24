159ef068289a18575c3ec27df8d5f0906fdecc55
pyavchik <pyavchik@gmail.com>
2018-11-24 16:44:32 +0200

    added other types of events

---
+++ Section16-Events/ex_4_type_of_events_todolist/app.js
@@ -0,0 +1,13 @@
+var firstLi = document.querySelector("li");
+
+firstLi.addEventListener("mouseover", function () {
+    console.log("Mouse over first li");
+    firstLi.style.color = "green";
+});
+
+firstLi.addEventListener("mouseout", function () {
+    console.log("Mouse over first li");
+    firstLi.style.color = "green";
+});
+
+
