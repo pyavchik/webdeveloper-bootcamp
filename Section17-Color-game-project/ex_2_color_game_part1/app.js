4beebfa65f1d3bc1507c8e1f805d390f9e0c70d8
pyavchik <pyavchik@gmail.com>
2018-11-25 23:36:22 +0200

    finished first part of color game

--- Section17-Color-game-project/ex_2_color_game_part1/app.js
+++ Section17-Color-game-project/ex_2_color_game_part1/app.js
@@ -8,6 +8,26 @@ let colors = [
 ];
 
 let squares = document.querySelectorAll(".square");
+let pickedColor = colors[3]
+let colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
 for (let i = 0; i < squares.length; i++) {
+    // add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    // add click listeners to squares
+
+    squares[i].addEventListener("click", function () {
+        // grab color of clicked square
+        let clickedColor = this.style.backgroundColor;
+
+        // compare color to picked color
+        if(clickedColor === pickedColor){
+            alert("Correct!");
+        } else {
+            alert("Wrong!");
+        }
+    })
 }
