ca6ec1f37018fded70e8025d3d46c099bd0169a1
pyavchik <pyavchik@gmail.com>
2018-11-28 23:27:28 +0200

    finished part 3 of color game

--- Section17-Color-game-project/ex_3_color_game_part3/app.js
+++ Section17-Color-game-project/ex_3_color_game_part3/app.js
@@ -1,16 +1,10 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-];
+let colors = generateRandomColors(6);
 
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
+let h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -28,6 +22,7 @@ for (let i = 0; i < squares.length; i++) {
         if(clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try again!";
@@ -47,4 +42,30 @@ function pickColor() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
 
+}
+
+function generateRandomColors(num) {
+    // make an array
+    var arr = [];
+
+    // add num random colors to array
+    for (let i = 0; i < num; i++) {
+        // get ranoom color and push to arr
+        arr.push(randomColor());
+    }
+
+    // return that array
+    return arr;
+}
+
+function randomColor() {
+    // pick a "red" from 0 - 255
+    var r = Math.floor(Math.random() * 256);
+
+    // pick a "green" from 0 - 255
+    var g = Math.floor(Math.random() * 256);
+
+    // pick a "blue" from 0 - 255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
