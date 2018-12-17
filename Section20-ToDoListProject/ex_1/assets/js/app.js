427e9ccf4ac307804379889d9d345497e6d8f93b
pyavchik <pyavchik@gmail.com>
2018-12-17 23:15:07 +0200

    deleting todos added

--- Section20-ToDoListProject/ex_1/assets/js/app.js
+++ Section20-ToDoListProject/ex_1/assets/js/app.js
@@ -1,21 +1,14 @@
 // check off Secific Todos by clicking
 
 $("li").click(function () {
-    // //if li is gray
-    // if($(this).css("color") === "rgb(128, 128, 128)") {
-    //     //turn it black
-    //     $(this).css({
-    //         color: "black",
-    //         textDecoration: "none"
-    //     });
-    // } else {
-    //     $(this).css({
-    //         color: "gray",
-    //         textDecoration: "line-through"
-    //     });
-    // }
-
     $(this).toggleClass("completed");
 });
 
+// Click on X to delete Todo
+$("span").click(function (e) {
+    $(this).parent().fadeOut(500,function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
 
