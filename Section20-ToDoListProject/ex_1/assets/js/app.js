1e4a35733c253473e8aca7b4fb79a3762ed1f090
pyavchik <pyavchik@gmail.com>
2018-12-17 22:25:31 +0200

    added marking completation

--- Section20-ToDoListProject/ex_1/assets/js/app.js
+++ Section20-ToDoListProject/ex_1/assets/js/app.js
@@ -1,6 +1,19 @@
 // check off Secific Todos by clicking
+
 $("li").click(function () {
-    alert("Clicked LI");
+    //if li is gray
+    if($(this).css("color") === "rgb(128, 128, 128)") {
+        //turn it black
+        $(this).css({
+            color: "black",
+            textDecoration: "none"
+        });
+    } else {
+        $(this).css({
+            color: "gray",
+            textDecoration: "line-through"
+        });
+    }
 });
 
 
