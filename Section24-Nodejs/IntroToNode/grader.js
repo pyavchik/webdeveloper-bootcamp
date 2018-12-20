ba37b4a6e5a59fede36c2c45eb832e37558ac1f3
pyavchik <pyavchik@gmail.com>
2018-12-20 23:02:49 +0200

    homework2

---
+++ Section24-Nodejs/IntroToNode/grader.js
@@ -0,0 +1,14 @@
+function average(arr) {
+    var score = 0;
+    for (let i = 0; i < arr.length; i++) {
+        score = score + arr[i]
+    }
+    averageNum = score / arr.length;
+    console.log(Math.round(averageNum));
+}
+
+var scores = [90, 98, 89, 100, 100, 86, 94];
+average(scores);
+
+var scores = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+average(scores);
