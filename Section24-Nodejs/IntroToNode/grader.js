32307f5748d707fd479f9ad4178eeaf4a1ef10d1
pyavchik <pyavchik@gmail.com>
2018-12-20 23:09:16 +0200

    refactoring home work2

--- Section24-Nodejs/IntroToNode/grader.js
+++ Section24-Nodejs/IntroToNode/grader.js
@@ -1,14 +1,17 @@
-function average(arr) {
-    var score = 0;
-    for (let i = 0; i < arr.length; i++) {
-        score = score + arr[i]
-    }
-    averageNum = score / arr.length;
-    console.log(Math.round(averageNum));
+function average(scores) {
+    var total = 0;
+    scores.forEach(function (score) {
+        total += score;
+    });
+
+    var avg = total / scores.length;
+
+    return Math.round(avg);
+
 }
 
 var scores = [90, 98, 89, 100, 100, 86, 94];
-average(scores);
+console.log(average(scores));
 
 var scores = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
-average(scores);
+console.log(average(scores));
