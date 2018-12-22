ef5396d7a97e522bc5e774e3a8753b23bd5155db
pyavchik <pyavchik@gmail.com>
2018-12-22 21:34:10 +0200

    route params example added

--- Section24-Nodejs/FirstExpressApp/app.js
+++ Section24-Nodejs/FirstExpressApp/app.js
@@ -17,6 +17,21 @@ app.get("/dog", function (req, res) {
     res.send("MEOW");
 });
 
+app.get("/r/:subredditName", function (req, res){
+    var subreddit = req.params.subredditName;
+    res.send("Welcome to a " + subreddit.toUpperCase() + " subreddit")
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function (req, res){
+    console.log(req.params);
+    res.send("Welcome to the comments page")
+});
+
+// "any other" => "404"
+app.get("*", function (req, res) {
+    res.send("404");
+});
+
 // Tell Express to listen for requests (start server)
 app.listen(3000, function () {
     console.log("Staruting server on port 3000");
