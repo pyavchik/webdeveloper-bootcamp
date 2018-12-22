08fc2496dc429d024f82e5d5465528248e87aa42
pyavchik <pyavchik@gmail.com>
2018-12-22 17:43:48 +0200

    first Express app added

--- Section24-Nodejs/FirstExpressApp/app.js
+++ Section24-Nodejs/FirstExpressApp/app.js
@@ -1,1 +1,23 @@
-console.log("First Express App");
+var express = require("express");
+
+var app = express();
+
+// "/" => "Hi there"
+app.get("/", function (req, res) {
+    res.send("Hi there!");
+});
+
+// "/bye" => "Goodbye"
+app.get("/bye", function (req, res) {
+    res.send("Goodbye");
+});
+
+// "/dog" => "MEOW"
+app.get("/dog", function (req, res) {
+    res.send("MEOW");
+});
+
+// Tell Express to listen for requests (start server)
+app.listen(3000, function () {
+    console.log("Staruting server on port 3000");
+});
