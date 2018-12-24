5faf755086d41fa40027f95273953d8a23d46c8b
pyavchik <pyavchik@gmail.com>
2018-12-24 23:15:28 +0200

    ejs conditionals and loops example

--- Section24-Nodejs/MoreExpress/EJSDemo/index.js
+++ Section24-Nodejs/MoreExpress/EJSDemo/index.js
@@ -10,6 +10,15 @@ app.get("/fallinlovewith/:thing", function (req, res) {
     res.render("love.ejs", {thingVar:thing});
 });
 
+app.get("/posts", function (req, res) {
+    var posts = [
+        {title: "Post1", autor: "Susy"},
+        {title: "My adorable pet bunny", autor: "Charlie"},
+        {title: "Can you believe this pomsky", autor: "Colt"},
+    ];
+    res.render("posts.ejs", {posts: posts});
+});
+
 app.listen(3000, function () {
     console.log("Server is listening port 3000");
 });
