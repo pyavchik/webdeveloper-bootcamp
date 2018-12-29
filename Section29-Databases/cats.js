287ba40d76458b5be1e194d47e1c0c06286cd9ef
pyavchik <pyavchik@gmail.com>
2018-12-29 18:51:02 +0200

    added mongoose part2

--- Section29-Databases/cats.js
+++ Section29-Databases/cats.js
@@ -11,5 +11,54 @@ var catSchema = new mongoose.Schema({
 var Cat = mongoose.model("Cat", catSchema);
 
 // adding a new cat to database
+// var george = new Cat({
+//     name: "George",
+//     age: 11,
+//     temperament: "Grouchy"
+// });
+// george.save(function (err, cat) {
+//     if(err){
+//         console.log("something went wrong!!!");
+//     } else {
+//         console.log("we just saved a cat to the db");
+//         console.log(cat);
+//     }
+// });
+
+// var mrsNorris = new Cat({
+//     name: "Mrs Norris",
+//     age: 7,
+//     temperament: "Evil"
+// });
+//
+// mrsNorris.save(function (err, cat) {
+//     if(err){
+//         console.log("something went wrong!!!");
+//     } else {
+//         console.log("we just saved a cat to the db");
+//         console.log(cat);
+//     }
+// });
+
+Cat.create({
+    name: "Snow white",
+    age: 15,
+    temperament: "Bland"
+}, function (err, cat) {
+    if(err){
+        console.log("err");
+    } else {
+        console.log(cat);
+    }
+});
 
 // retrieve all cats from the DB and console.log each one
+Cat.find({}, function (err, cats) {
+    if(err){
+        console.log("oh no, error!");
+        console.log(err);
+    } else {
+        console.log("All the cats ...");
+        console.log(cats);
+    }
+});
