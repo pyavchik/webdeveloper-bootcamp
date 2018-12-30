e2a444818f2e892e4a664b2882e17c8086b8315d
pyavchik <pyavchik@gmail.com>
2018-12-30 13:00:48 +0200

    added show page

--- Section28-YelpCamp/YelpCampV2/app.js
+++ Section28-YelpCamp/YelpCampV2/app.js
@@ -9,8 +9,9 @@ app.set("view engine", "ejs");
 
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
-   name: String,
-   image: String
+    name: String,
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -18,7 +19,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 //     {
 //         name: "Granite Hill",
-//         image: "https://assets.simpleviewcms.com/simpleview/image/upload/c_fill,h_571,q_80,w_1603/v1/clients/raleigh/165_3_0358_jpeg_7b14f35b-cc84-4145-88f1-0c727e17aaa4.jpg"
+//         image: "https://www.planetware.com/photos-large/USSD/south-dakota-black-hills-national-forest-horsethief-lake-campground-tent-site.jpg",
+//         description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"
 //     }, function (err, campground) {
 //         if (!err){
 //             console.log("Newly created campground");
@@ -29,20 +31,6 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 //     }
 // );
 
-// var campgrounds = [
-//     {name: "Salmon Creek", image: "https://assets.simpleviewcms.com/simpleview/image/upload/c_fill,h_571,q_80,w_1603/v1/clients/raleigh/165_3_0358_jpeg_7b14f35b-cc84-4145-88f1-0c727e17aaa4.jpg"},
-//     {name: "Granite Hill", image: "https://media-cdn.tripadvisor.com/media/photo-s/0e/df/88/a0/our-spacious-campsite.jpg"},
-//     {name: "Mountain Goat's Rest", image: "https://travel.home.sndimg.com/content/dam/images/travel/fullrights/2016/01/14/national-park-camping/camping-glacier-national-park-camping.jpg.rend.hgtvcom.966.725.suffix/1491593018562.jpeg"},
-//     {name: "Salmon Creek", image: "https://assets.simpleviewcms.com/simpleview/image/upload/c_fill,h_571,q_80,w_1603/v1/clients/raleigh/165_3_0358_jpeg_7b14f35b-cc84-4145-88f1-0c727e17aaa4.jpg"},
-//     {name: "Granite Hill", image: "https://media-cdn.tripadvisor.com/media/photo-s/0e/df/88/a0/our-spacious-campsite.jpg"},
-//     {name: "Mountain Goat's Rest", image: "https://travel.home.sndimg.com/content/dam/images/travel/fullrights/2016/01/14/national-park-camping/camping-glacier-national-park-camping.jpg.rend.hgtvcom.966.725.suffix/1491593018562.jpeg"},
-//     {name: "Salmon Creek", image: "https://assets.simpleviewcms.com/simpleview/image/upload/c_fill,h_571,q_80,w_1603/v1/clients/raleigh/165_3_0358_jpeg_7b14f35b-cc84-4145-88f1-0c727e17aaa4.jpg"},
-//     {name: "Granite Hill", image: "https://media-cdn.tripadvisor.com/media/photo-s/0e/df/88/a0/our-spacious-campsite.jpg"},
-//     {name: "Mountain Goat's Rest", image: "https://travel.home.sndimg.com/content/dam/images/travel/fullrights/2016/01/14/national-park-camping/camping-glacier-national-park-camping.jpg.rend.hgtvcom.966.725.suffix/1491593018562.jpeg"},
-//     {name: "Salmon Creek", image: "https://assets.simpleviewcms.com/simpleview/image/upload/c_fill,h_571,q_80,w_1603/v1/clients/raleigh/165_3_0358_jpeg_7b14f35b-cc84-4145-88f1-0c727e17aaa4.jpg"},
-//     {name: "Granite Hill", image: "https://media-cdn.tripadvisor.com/media/photo-s/0e/df/88/a0/our-spacious-campsite.jpg"},
-//     {name: "Mountain Goat's Rest", image: "https://travel.home.sndimg.com/content/dam/images/travel/fullrights/2016/01/14/national-park-camping/camping-glacier-national-park-camping.jpg.rend.hgtvcom.966.725.suffix/1491593018562.jpeg"},
-// ];
 
 app.get("/", function (req, res) {
     res.render("landing");
@@ -52,7 +40,7 @@ app.get("/campgrounds", function (req, res) {
     // Get all campgrounds from DB
     Campground.find({}, function (err, allCampgrounds) {
         if(!err){
-            res.render("campgrounds", {campgrounds:allCampgrounds});
+            res.render("index", {campgrounds:allCampgrounds});
         } else {
             console.log(err);
         }
@@ -80,6 +68,11 @@ app.post("/campgrounds", function (req, res) {
 
 app.get("/campgrounds/new", function (req, res) {
     res.render("new.ejs");
+});
+
+// SHOW -shows more info about one campground
+app.get("/campgrounds/:id", function (req, res) {
+    res.render("show.ejs");
 });
 
 app.listen(3000, function () {
