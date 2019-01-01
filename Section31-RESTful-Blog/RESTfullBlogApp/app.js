593f33aae8cca9b7b4b00bb02e4ccd3dc2b19c90
pyavchik <pyavchik@gmail.com>
2019-01-01 22:51:15 +0200

    added show.ejs

--- Section31-RESTful-Blog/RESTfullBlogApp/app.js
+++ Section31-RESTful-Blog/RESTfullBlogApp/app.js
@@ -56,6 +56,17 @@ app.post("/blogs",function (req, res) {
     });
 });
 
+// SHOW ROUTE
+app.get("/blogs/:id", function (req, res) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
