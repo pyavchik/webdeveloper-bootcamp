cf18d913ff0e6e832b86a4cd822169592e9d706f
pyavchik <pyavchik@gmail.com>
2019-01-03 18:20:07 +0200

    finished restfull blog

--- Section31-RESTful-Blog/RESTfullBlogApp/app.js
+++ Section31-RESTful-Blog/RESTfullBlogApp/app.js
@@ -1,14 +1,18 @@
-var  bodyParser    = require("body-parser"),
-    methodOverride = require("method-override"),
-    mongoose       = require("mongoose"),
-    express        = require("express"),
-    app            = express();
+var  bodyParser      = require("body-parser"),
+    methodOverride   = require("method-override"),
+    expressSanitizer = require("express-sanitizer"),
+    mongoose         = require("mongoose"),
+    express          = require("express"),
+    app              = express();
+
 
 // App config
+mongoose.set("useFindAndModify", false);
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 // Mondgoose/Model config
@@ -49,6 +53,8 @@ app.get("/blogs/new", function (req, res) {
 // CREATE ROUTE
 app.post("/blogs",function (req, res) {
     // create blog
+    console.log(req.body);
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function (err, newBlog) {
         if(err){
             res.render("new");
@@ -82,6 +88,7 @@ app.get("/blogs/:id/edit", function (req, res) {
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function (req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog) {
         if(err){
             res.redirect("/blogs");
