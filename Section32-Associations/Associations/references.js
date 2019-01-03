045fff7362a91ddd727ecd5d275357145ad8aab8
pyavchik <pyavchik@gmail.com>
2019-01-03 20:42:26 +0200

    object references example added

--- Section32-Associations/Associations/references.js
+++ Section32-Associations/Associations/references.js
@@ -1,47 +1,29 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/blog_demo_2", {useNewUrlParser: true});
 
-// post - title, content
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-});
+var Post = require("./models/post");
+var User = require("./models/user");
 
-// user - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Post"
+Post.create({
+    title: "How to cook the best burger part 5",
+    content: "asdfasdfasdfasd  as dfasdf adsofia sdoifaosdfnewcontent5"
+}, function (err, post) {
+    User.findOne({email: "bob@gamil.com"}, function (err, foundUser) {
+        if (err) {
+            console.log(err)
+        } else {
+            foundUser.posts.push(post);
+            foundUser.save(function (err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(data);
+                }
+            })
         }
-    ]
+    });
+
 });
-var User = mongoose.model("User", userSchema);
-
-var Post = mongoose.model("Post", postSchema);
-
-// Post.create({
-//     title: "How to cook the best burger part 4",
-//     content: "asdfasdfasdf asdf asd fa sdf a sd f as"
-// }, function (err, post) {
-//     User.findOne({email: "bob@gamil.com"}, function (err, foundUser) {
-//         if (err) {
-//             console.log(err)
-//         } else {
-//             foundUser.posts.push(post);
-//             foundUser.save(function (err, data) {
-//                 if (err) {
-//                     console.log(err);
-//                 } else {
-//                     console.log(data);
-//                 }
-//             })
-//         }
-//     });
-//
-// });
 
 // User.create({
 //     email: "bob@gamil.com",
@@ -52,10 +34,10 @@ var Post = mongoose.model("Post", postSchema);
 // Find user
 // Find all posts for that user
 
-User.findOne({email: "bob@gamil.com"}).populate("posts").exec(function (err, user) {
-    if(err){
-        console.log(err);
-    } else {
-        console.log(user);
-    }
-});
+// User.findOne({email: "bob@gamil.com"}).populate("posts").exec(function (err, user) {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(user);
+//     }
+// });
