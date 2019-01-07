1f849cf83ff9bb8c146c92f29dbe17fcca3eda06
pyavchik <pyavchik@gmail.com>
2019-01-07 11:46:40 +0200

    added path to public folder

--- Section28-YelpCamp/YelpCampV2/app.js
+++ Section28-YelpCamp/YelpCampV2/app.js
@@ -11,6 +11,7 @@ var Comment = require("./models/comment");
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 app.get("/", function (req, res) {
