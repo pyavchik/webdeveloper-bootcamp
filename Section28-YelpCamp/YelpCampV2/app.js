1d3494e935b63aad129eea755c331acc19f7fc47
pyavchik <pyavchik@gmail.com>
2019-01-22 21:38:36 +0200

    added edit campground

--- Section28-YelpCamp/YelpCampV2/app.js
+++ Section28-YelpCamp/YelpCampV2/app.js
@@ -4,9 +4,7 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
-var Campground = require("./models/campground");
-var seedDB = require("./seeds");
-var Comment = require("./models/comment");
+var methodOverride = require("method-override");
 var User = require("./models/user");
 
 // requring routes
@@ -18,6 +16,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true})
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 //seedDB(); // seed the database
 
 // PASSPORT CONFIGURATION
