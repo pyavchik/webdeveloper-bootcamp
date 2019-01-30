42c372c4a4d2d4341c6087de1430eaab277cc3c3
pyavchik@gmail.com <pyavchik@gmail.com>
2019-01-30 23:38:42 +0200

    update comment.js

--- Section28-YelpCamp/YelpCampV2/models/comment.js
+++ Section28-YelpCamp/YelpCampV2/models/comment.js
@@ -3,8 +3,8 @@ var mongoose = require("mongoose");
 var commentSchema = new mongoose.Schema({
     text: String,
     author: {
-        id:{
-            type:mongoose.Schema.Types.ObjectId,
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
             ref: "User"
         },
         username: String
