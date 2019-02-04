f5bb362bc3c1d6daa5811575bf97458a9ee88940
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-04 22:46:44 +0200

    added process.env.database

--- Section28-YelpCamp/YelpCampV2/app.js
+++ Section28-YelpCamp/YelpCampV2/app.js
@@ -13,8 +13,7 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
-//mongoose.connect(process.env.databaseURL, {useNewUrlParser: true});
+mongoose.connect(process.env.databaseURL, {useNewUrlParser: true});
 
 
 app.use(bodyParser.urlencoded({extended: true}));
